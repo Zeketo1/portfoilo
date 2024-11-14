4b49f4df61df07ef997fa7515cbba196479d5116
zeketo <ikorofrancis24@gmail.com>
2024-11-14 21:02:04 +0100

    again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Logo from "@/common/Logo";
 import { HoverBorderGradient } from "@/components/ui/button";
